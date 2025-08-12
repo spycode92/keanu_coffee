7a19fa98618423b8c71a4ba831549780600d01e2
박민경 <alszhd4085@naver.com>
2025-08-12 20:34:12 +0900

    inventory/재고관리 대시보드 초안

--- src/main/java/com/itwillbs/keanu_coffee/inventory/controller/InventoryController.java
+++ src/main/java/com/itwillbs/keanu_coffee/inventory/controller/InventoryController.java
@@ -1,8 +1,19 @@
 package com.itwillbs.keanu_coffee.inventory.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import lombok.RequiredArgsConstructor;
 
 @Controller
+@RequiredArgsConstructor
+@RequestMapping("/inventory")
 public class InventoryController {
-
+	
+	// 재고현황 페이지 - 대시보드
+	@GetMapping("")
+	public String inventoryDashboard() {
+		return "inventory/inventoryDashboard";
+	}
 }
