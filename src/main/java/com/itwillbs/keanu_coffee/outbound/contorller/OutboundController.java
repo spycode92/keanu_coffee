d39cfa1cc0f21793e660ab69d49e93b3be306dcc
배수현 <alwaysone46@gmail.com>
2025-08-13 17:38:24 +0900

    inbound&outbound / view작업

--- src/main/java/com/itwillbs/keanu_coffee/outbound/contorller/OutboundController.java
+++ src/main/java/com/itwillbs/keanu_coffee/outbound/contorller/OutboundController.java
@@ -1,8 +1,24 @@
 package com.itwillbs.keanu_coffee.outbound.contorller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import lombok.RequiredArgsConstructor;
 
 @Controller
+@RequiredArgsConstructor
+@RequestMapping("/outbound")
 public class OutboundController {
-
+	
+	@GetMapping("/main")
+	public String showOutboundDashboard() {
+	    return "/outbound/outboundDashboard";
+	}
+	
+	@GetMapping("/outboundManagement")
+	public String showOutboundManagement() {
+	    return "/outbound/outboundManagement";
+	}
+	
 }
