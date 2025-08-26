546676131ca13b8fedd925d0bc504aac1821af5b
이정민 <yoyo3302@gmail.com>
2025-08-26 17:41:55 +0900

    transport/ 운전자 수 카운트를 위한 DriverMapper 작성

--- src/main/java/com/itwillbs/keanu_coffee/transport/mapper/DriverMapper.java
+++ src/main/java/com/itwillbs/keanu_coffee/transport/mapper/DriverMapper.java
@@ -2,11 +2,16 @@ package com.itwillbs.keanu_coffee.transport.mapper;
 
 import java.util.List;
 
+import org.apache.ibatis.annotations.Param;
+
 import com.itwillbs.keanu_coffee.admin.dto.EmployeeInfoDTO;
 
 public interface DriverMapper {
 
 	// 차량 배정 가능한 기사 리스트
 	List<EmployeeInfoDTO> selectDriverList();
+
+	// 페이징을 위한 운전사 수
+	int selectDriverCount(@Param("filter") String filter, @Param("searchKeyword") String searchKeyword);
 
 }
