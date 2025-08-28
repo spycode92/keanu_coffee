ec59104d6ee6161f2041e59288c62f711783bb36
Isak <isakshavingfun@gmail.com>
2025-08-28 10:38:29 +0900

    Isak
    inventory to move update

--- src/main/java/com/itwillbs/keanu_coffee/inventory/mapper/InventoryMapper.java
+++ src/main/java/com/itwillbs/keanu_coffee/inventory/mapper/InventoryMapper.java
@@ -1,8 +1,16 @@
 package com.itwillbs.keanu_coffee.inventory.mapper;
 
+import java.util.List;
+
 import org.apache.ibatis.annotations.Mapper;
+
+import com.itwillbs.keanu_coffee.inventory.dto.InventoryDTO;
 
 @Mapper
 public interface InventoryMapper {
+
+//	List<InventoryDTO> selectInventoryThatNeedsToMoveFromInbound();
+//
+//	List<InventoryDTO> selectInventoryThatNeedsToMoveToOutbound();
 
 }
