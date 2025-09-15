776453a3870708f0dc552bea5d857c44a65eb6e5
Isak <isakshavingfun@gmail.com>
2025-09-15 09:39:36 +0900

    Isak
    move inventory partially working

--- src/main/java/com/itwillbs/keanu_coffee/inventory/service/InventoryActionsService.java
+++ src/main/java/com/itwillbs/keanu_coffee/inventory/service/InventoryActionsService.java
@@ -1,5 +1,6 @@
 package com.itwillbs.keanu_coffee.inventory.service;
 
+import java.sql.Date;
 import java.util.List;
 
 import org.apache.ibatis.annotations.Param;
@@ -7,7 +8,9 @@ import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import com.itwillbs.keanu_coffee.inbound.dto.ReceiptProductDTO;
 import com.itwillbs.keanu_coffee.inventory.dto.InventoryDTO;
+import com.itwillbs.keanu_coffee.inventory.dto.ReceiptProductDTO2;
 import com.itwillbs.keanu_coffee.inventory.dto.WarehouseLocationDTO;
 import com.itwillbs.keanu_coffee.inventory.mapper.InventoryActionsMapper;
 
@@ -52,5 +55,42 @@ public class InventoryActionsService {
 		inventoryActionsMapper.updateLocationOfInventory(inventoryId, qtyMoving, employeeId);
 		
 	}
+
+	public void addLocationOfInventory(InventoryDTO inventoryDTO, int qtyMoving, int employeeId) {
+		System.out.println("inventoryDTO : " + inventoryDTO);
+		System.out.println("employeeId : " + employeeId);
+		inventoryActionsMapper.insertLocationOfInventory(inventoryDTO, qtyMoving, employeeId);
+		
+	}
+
+	public void modifyLocationOfInventory2(int inventoryId, int qtyMoving, String destinationName, int locationIdxOfDestinationName) {
+		
+		inventoryActionsMapper.updateLocationOfInventory2(inventoryId, qtyMoving, destinationName, locationIdxOfDestinationName);
+	}
+
+//	public int getlocationIdxOfDestinationName(String destinationName) {
+	public String getlocationIdxOfDestinationName(String destinationName) {
+		return inventoryActionsMapper.selectlocationIdxOfDestinationName(destinationName);
+	}
+
+	public void addLocationOfInventory2(InventoryDTO inventoryDTO, int qtyMoving, String destinationName,
+			int locationIdxOfDestinationName) {
+		inventoryActionsMapper.insertLocationOfInventory2(inventoryDTO, qtyMoving, destinationName, locationIdxOfDestinationName);
+	}
+
+	public ReceiptProductDTO2 getReceiptProduct(int receiptID) {
+		// TODO Auto-generated method stub
+		return inventoryActionsMapper.selectReceiptProduct(receiptID);
+	}
+
+	public void addLocationOfInventory3(int receiptProductIdx, int locationIdxOfDestinationName, String destinationName,
+			int productIdx, int qtyMoving, String lotNumber, Date manufactureDate, Date expirationDate) {
+		
+		inventoryActionsMapper.insertLocationOfInventory3(receiptProductIdx, locationIdxOfDestinationName, destinationName,
+				productIdx, qtyMoving, lotNumber, manufactureDate, expirationDate);
+		
+	}
+
+	
 	
 }
