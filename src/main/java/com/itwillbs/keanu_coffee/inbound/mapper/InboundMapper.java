30f816098ecc0be4ee4e81fa877864f4e29a26a8
wenir446 <alwaysone46@gmail.com>
2025-09-18 08:23:18 +0900

    inbound / excel

--- src/main/java/com/itwillbs/keanu_coffee/inbound/mapper/InboundMapper.java
+++ src/main/java/com/itwillbs/keanu_coffee/inbound/mapper/InboundMapper.java
@@ -1,15 +1,12 @@
 package com.itwillbs.keanu_coffee.inbound.mapper;
 
 import java.util.List;
+import java.util.Map;
 
 import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Param;
 
 import com.itwillbs.keanu_coffee.admin.dto.EmployeeInfoDTO;
-import com.itwillbs.keanu_coffee.admin.dto.ProductDTO;
-import com.itwillbs.keanu_coffee.common.dto.PurchaseOrderDTO;
-import com.itwillbs.keanu_coffee.common.dto.PurchaseOrderItemDTO;
-import com.itwillbs.keanu_coffee.inbound.controller.InboundController.EmployeeOption;
 import com.itwillbs.keanu_coffee.inbound.dto.InboundDetailDTO;
 import com.itwillbs.keanu_coffee.inbound.dto.InboundManagementDTO;
 import com.itwillbs.keanu_coffee.inbound.dto.InboundProductDetailDTO;
@@ -18,8 +15,14 @@ import com.itwillbs.keanu_coffee.inbound.dto.ReceiptProductDTO;
 @Mapper
 public interface InboundMapper {
 	
-	// management 전체 리스트 조회
-	List<InboundManagementDTO> selectInboundWaitingInfo();
+	// management 리스트 조회 (페이징)
+	List<InboundManagementDTO> selectInboundList(Map<String, Object> searchParams);
+	// management 리스트 조회 (엑셀)
+	List<InboundManagementDTO> selectInboundListForExcel(Map<String, Object> searchParams);
+	
+	// management 총 개수
+	int selectInboundCount(Map<String, Object> searchParams);
+	
 	
 	// detail 기본 정보 조회
 	InboundDetailDTO selectInboundDetailData(int ibwaitIdx);
@@ -27,22 +30,25 @@ public interface InboundMapper {
 	// detail 상품 정보 조회 
 	List<InboundProductDetailDTO> selectInboundProductDetail(String orderNumber);
 	
-	// detail 로케이션 인서트&업데이트
-	void updateLocation(@Param("ibwaitIdx") Long ibwaitIdx, @Param("inboundLocation") String inboundLocation);
+	// detail 로케이션 수정
+	void updateLocation(@Param("ibwaitIdx") Long ibwaitIdx,
+	                    @Param("inboundLocation") String inboundLocation);
 
+	// 매니저 조회/수정
 	List<EmployeeInfoDTO> selectEmployeeList();
-
-	void updateManager(@Param("ibwaitIdx") Long ibwaitIdx, @Param("managerName") String managerName);
+	void updateManagers(@Param("ibwaitIdxList") List<Integer> ibwaitIdxList,
+			            @Param("managerIdx") Long managerIdx,
+			            @Param("managerName") String managerName);
 	
 	// 검수 데이터 여부 확인
-	boolean selectDataExists(@Param("ibwaitIdx") Long ibwaitIdx, @Param("productIdx") Long productIdx, @Param("lotNumber") String lotNumber);
+	int selectDataExists(@Param("ibwaitIdx") Long ibwaitIdx,
+	                     @Param("productIdx") Long productIdx,
+	                     @Param("lotNumber") String lotNumber);
 	
-	// 검수완료 데이터 업데이트
+	// 검수완료 데이터 저장/수정
 	void updateReceiptProduct(ReceiptProductDTO dto);
 	void insertReceiptProduct(ReceiptProductDTO dto);
 	void updatePurchaseOrderItemAfterInspection(ReceiptProductDTO dto);
-	
-	
 
-
+	
 }
--- src/main/java/com/itwillbs/keanu_coffee/inbound/service/InboundService.java
+++ src/main/java/com/itwillbs/keanu_coffee/inbound/service/InboundService.java
@@ -1,18 +1,18 @@
 package com.itwillbs.keanu_coffee.inbound.service;
 
+
 import java.time.format.DateTimeFormatter;
 import java.util.List;
+import java.util.Map;
 
+import org.apache.poi.ss.usermodel.*;
+import org.apache.poi.ss.usermodel.Workbook;
+import org.apache.poi.ss.util.CellRangeAddress;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import com.itwillbs.keanu_coffee.admin.dto.EmployeeInfoDTO;
-import com.itwillbs.keanu_coffee.admin.dto.ProductDTO;
-import com.itwillbs.keanu_coffee.common.dto.PurchaseOrderDTO;
-import com.itwillbs.keanu_coffee.common.dto.PurchaseOrderItemDTO;
-import com.itwillbs.keanu_coffee.common.dto.PurchaseWithSupplierDTO;
-import com.itwillbs.keanu_coffee.common.mapper.PurchaseOrderMapper;
-import com.itwillbs.keanu_coffee.inbound.controller.InboundController.EmployeeOption;
 import com.itwillbs.keanu_coffee.inbound.dto.InboundDetailDTO;
 import com.itwillbs.keanu_coffee.inbound.dto.InboundManagementDTO;
 import com.itwillbs.keanu_coffee.inbound.dto.InboundProductDetailDTO;
@@ -27,19 +27,26 @@ public class InboundService {
 	
 	private final InboundMapper inboundMapper;
 	
-	// management list 조회
-	public List<InboundManagementDTO> getAllinboundWaitingInfo() {
-		
-		List<InboundManagementDTO> orderDetailList = inboundMapper.selectInboundWaitingInfo();
-		
-		// LocalDateTime → String 포맷 처리
-	    DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm");
-	    orderDetailList.forEach(dto -> {
-	        if (dto.getArrivalDate() != null) {
-	            dto.setArrivalDateStr(dto.getArrivalDate().format(formatter));
-	        }
-	    });
-		
+	// management 페이징 전체 개수
+	public int getInboundCount(Map<String, Object> searchParams) {
+		return inboundMapper.selectInboundCount(searchParams);
+	}
+	
+	// management list 조회 (페이징 적용)
+	public List<InboundManagementDTO> getInboundList(Map<String, Object> searchParams, int startRow, int listLimit) {
+		searchParams.put("startRow", startRow);
+		searchParams.put("listLimit", listLimit);
+
+		List<InboundManagementDTO> orderDetailList = inboundMapper.selectInboundList(searchParams);
+
+		// 날짜 포맷팅
+		DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm");
+		orderDetailList.forEach(dto -> {
+			if (dto.getArrivalDate() != null) {
+				dto.setArrivalDateStr(dto.getArrivalDate().format(formatter));
+			}
+		});
+
 		return orderDetailList;
 	}
 
@@ -53,7 +60,7 @@ public class InboundService {
 		return inboundMapper.selectInboundProductDetail(orderNumber);
 	}
 	
-	// Detail 모달로 로케이션 수정
+	// Detail 로케이션 수정
 	public void updateLocation(Long ibwaitIdx, String inboundLocation) {
 		inboundMapper.updateLocation(ibwaitIdx, inboundLocation);
 	}
@@ -62,16 +69,18 @@ public class InboundService {
 		return inboundMapper.selectEmployeeList();
 	}
 
-	public void updateManager(Long ibwaitIdx, String managerName) {
-		inboundMapper.updateManager(ibwaitIdx, managerName);
+	@Transactional
+	public void updateManagers(List<Integer> ibwaitIdxList, Long managerIdx, String managerName) {
+	    if(ibwaitIdxList == null || ibwaitIdxList.isEmpty()) return;
+	    inboundMapper.updateManagers(ibwaitIdxList, managerIdx, managerName);
 	}
 	
 	// 검수완료 데이터 확인
 	public boolean findDataExists(Long ibwaitIdx, Long productIdx, String lotNumber) {
-		return inboundMapper.selectDataExists(ibwaitIdx, productIdx, lotNumber);
+		return inboundMapper.selectDataExists(ibwaitIdx, productIdx, lotNumber) > 0;
 	}
 	
-	// 데이터 수정
+	// 검수 완료시 데이터 저장/수정
 	@Transactional
 	public void inspectionCompleteUpdate(ReceiptProductDTO dto, boolean exists) {
 		if (exists) {
@@ -79,13 +88,9 @@ public class InboundService {
 		} else {
 			inboundMapper.insertReceiptProduct(dto);
 		}
-		// 가격/수량/LOT 반영
 	    inboundMapper.updatePurchaseOrderItemAfterInspection(dto);
 	}
 	
-	
 
 
-	
-	
 }
