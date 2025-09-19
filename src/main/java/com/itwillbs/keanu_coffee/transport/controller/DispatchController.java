7cb5cafcd5b301a997b42e9af13cd93662b2113d
jm-316 <yoyo3302@gmail.com>
2025-09-19 14:30:12 +0900

    DispatchController 사용안하는 import 삭제

--- src/main/java/com/itwillbs/keanu_coffee/transport/controller/DispatchController.java
+++ src/main/java/com/itwillbs/keanu_coffee/transport/controller/DispatchController.java
@@ -16,7 +16,6 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestPart;
 import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
 
 import com.itwillbs.keanu_coffee.common.security.EmployeeDetail;
