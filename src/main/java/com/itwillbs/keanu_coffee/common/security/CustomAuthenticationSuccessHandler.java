eef9df66fed988edce757c75044edb1fb9b22d8c
spycode92 <spycode92@gmail.com>
2025-09-24 13:51:11 +0900

    로그인 변경 , 입고 큐알코드 생성

--- src/main/java/com/itwillbs/keanu_coffee/common/security/CustomAuthenticationSuccessHandler.java
+++ src/main/java/com/itwillbs/keanu_coffee/common/security/CustomAuthenticationSuccessHandler.java
@@ -12,6 +12,8 @@ import javax.servlet.http.HttpSession;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
 
+import com.itwillbs.keanu_coffee.common.controller.RedirectDecider;
+
 // 스프링 시큐리티로 로그인 성공 시 작업을 처리할 핸들러
 // => AuthenticationSuccessHandler 구현체 형태로 정의
 public class CustomAuthenticationSuccessHandler implements AuthenticationSuccessHandler {
@@ -26,34 +28,37 @@ public class CustomAuthenticationSuccessHandler implements AuthenticationSuccess
 				) throws IOException, ServletException {
 		
 		// 인증 정보 중 아이디 가져오기
-		String empNo = authentication.getName();
+//		String empNo = authentication.getName();
 		// 인증 정보를 MyUserDetails 객체로 변환 => getPrincipal() 메서드로 Principal 객체 얻어와서 변환
-		EmployeeDetail employeeDetail = (EmployeeDetail)authentication.getPrincipal();
+//		EmployeeDetail employeeDetail = (EmployeeDetail)authentication.getPrincipal();
 		
-		String roleName = employeeDetail.getRole().getRoleName();
-		String redirectURL = "/main";
-		switch(roleName) {
-			case "최고관리자" : redirectURL = "/admin/dash";
-				break;
-			case "시스템관리자" : redirectURL = "/admin/systemPreference/dept";
-				break;
-			case "입고관리자" : redirectURL = "/inbound/main";
-				break;
-			case "입고평사원" : redirectURL = "/inbound/management";
-				break;
-			case "출고관리자" : redirectURL = "/outbound/main";
-				break;
-			case "출고평사원" : redirectURL = "/outbound/outboundManagement";
-				break;
-			case "재고관리자" : redirectURL = "/inventory/main";
-				break;
-			case "재고평사원" : redirectURL = "/inventory/moveInventory";
-				break;
-			case "운송관리자" : redirectURL = "/transport/main";
-				break;
-			case "운송기사" : redirectURL = "/transport/mypage/" + empNo;
-				break;
-		}
+//		String roleName = employeeDetail.getRole().getRoleName();
+//		String redirectURL = "/main";
+//		switch(roleName) {
+//			case "최고관리자" : redirectURL = "/admin/dash";
+//				break;
+//			case "시스템관리자" : redirectURL = "/admin/systemPreference/dept";
+//				break;
+//			case "입고관리자" : redirectURL = "/inbound/main";
+//				break;
+//			case "입고평사원" : redirectURL = "/inbound/management";
+//				break;
+//			case "출고관리자" : redirectURL = "/outbound/main";
+//				break;
+//			case "출고평사원" : redirectURL = "/outbound/outboundManagement";
+//				break;
+//			case "재고관리자" : redirectURL = "/inventory/main";
+//				break;
+//			case "재고평사원" : redirectURL = "/inventory/moveInventory";
+//				break;
+//			case "운송관리자" : redirectURL = "/transport/main";
+//				break;
+//			case "운송기사" : redirectURL = "/transport/mypage/" + empNo;
+//				break;
+//		}
+		
+		RedirectDecider redirectDecider = new RedirectDecider();
+		String redirectURL = redirectDecider.decideRedirectUrl(authentication);
 		response.sendRedirect(redirectURL);
 	}
 	
